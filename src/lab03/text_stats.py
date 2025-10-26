1288a8a17f1e516df0ac5eb67bf2dd6a81875442
xinqqi <m2500477@edu.misis.ru>
2025-10-27 02:33:22 +0300

    lab3

---
+++ src/lab03/text_stats.py
@@ -0,0 +1,21 @@
+import sys
+from lib.text import normalize, tokenize, count_freq, top_n
+
+def main():
+    text = sys.stdin.read()
+
+    text = normalize(text=text)
+    tokens = tokenize(text=text)
+    freq = count_freq(tokens)
+    top_5 = top_n(freq, 5)
+
+    print(f"Всего слов: {len(tokens)}")
+    print(f"Уникальных слов: {len(set(tokens))}")
+    print("Топ-5:")
+    for word, count in top_5:
+        print(f"{word}:{count}")
+
+if __name__ == "__main__":
+    main()
+
+# python -m src.lab03.text_stats
