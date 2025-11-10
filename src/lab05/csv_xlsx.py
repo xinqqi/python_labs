c8c4e0bb9c7daed2a827f116d8f560e54ec0ca95
xinqqi <m2500477@edu.misis.ru>
2025-11-10 23:28:53 +0300

    red

--- src/lab05/csv_xlsx.py
+++ src/lab05/csv_xlsx.py
@@ -5,7 +5,7 @@ from pathlib import Path
 def csv_to_xlsx(csv_path: str, xlsx_path: str) -> None:
     """
     Конвертирует CSV в XLSX.
-    Использовать openpyxl ИЛИ xlsxwriter.
+    Буду использовать openpyxl.
     Первая строка CSV — заголовок.
     Лист называется "Sheet1".
     Колонки — автоширина по длине текста (не менее 8 символов).
